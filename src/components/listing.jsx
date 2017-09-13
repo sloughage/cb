9b584355bc9e428ba40cef8832aef8cc2f24e395
sloughage <lanesailer@gmail.com>
2017-09-13 17:37:14 -0500

    9/13

--- src/components/listing.jsx
+++ src/components/listing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default (props) =>
   <div>
+    <p>sold by: {props.listing.username}</p>
     <p>{props.listing.title}</p>
     <p>{props.listing.by.join(', ')}</p>
     <p>{props.listing.tag.join(', ')}</p>
